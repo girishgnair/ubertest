733e500643afdc5f1c079f9d2b665b7c940185c1
gnair <gnair@zynga.com>
2014-03-04 13:22:51 -0800

    ADded logging

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ import os
 #Config of where the database should be setup
 basedir = os.path.abspath(os.path.dirname(__file__))
 if os.environ.get('DATABASE_URL') is None:
+	print "DID NOT FIND DATABASE URL"
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
+	print "FOUND DATABASE URL"
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
