c4d0c83b1b4539579e12ebd19f992d0a8d0a7a70
gnair <gnair@zynga.com>
2014-03-07 09:00:31 -0800

    Fixing changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.httpauth import HTTPBasicAuth
 from flask.ext.sqlalchemy import SQLAlchemy
 
 #Initialization of the script. Initializing authentication and SQLAlchemy objects
-app = Flask(__name__, static_url_path = "")
+app = Flask(__name__)
 app.config.from_object('config')
 auth = HTTPBasicAuth()
 db = SQLAlchemy(app)
