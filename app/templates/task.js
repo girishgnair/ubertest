ef0f39de6ebe9ac7580c52de06a496155369c6b1
gnair <gnair@zynga.com>
2014-03-10 11:14:02 -0700

    Movin js out

--- app/templates/task.js
+++ app/templates/task.js
@@ -1,6 +1,6 @@
 function TasksViewModel() {
             var self = this;
-            self.tasksURI = 'http://localhost:8000/todo/api/v1.0/tasks';
+            self.tasksURI = 'http://girishubertest.herokuapp.com/todo/api/v1.0/tasks';
             self.username = "";
             self.password = "";
             self.tasks = ko.observableArray();
